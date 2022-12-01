11e4edf88be4ca79727ce15f2df9ba7591e8d8a5
egxn <afvelosas@correo.udistrital.edu.co>
2022-11-30 21:26:50 -0500

    🍥 feat-api: websockets

--- server/tests/stt.test.py
+++ server/tests/stt.test.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://127.0.0.1:5000/api/stt'
-file = {'file': open('hello_cats_from_the_outer_outer_space.wav', 'rb')}
+file = {'file': open('audio.test.wav', 'rb')}
 resp = requests.post(url=url, files=file)
 print(resp.json())
---
+++ server/tests/tts.test.py
@@ -0,0 +1,7 @@
+# import sys
+# sys.path.append('../')
+
+# from tts_coqui import main
+# from asyncio import run
+
+# run(main())
